6066e934c04751219633436412067ca7ded6e5b8
Emmanuel Holguin <99060682+Eholguin05@users.noreply.github.com>
2022-05-08 00:57:35 -0500

    Create ExplorerController.test.js

---
+++ test/controllers/ExplorerController.test.js
@@ -0,0 +1,95 @@
+const ExplorerController = require("./../../lib/controllers/ExplorerController");
+
+describe("Unit Tests for ExplorerController class", () => {
+  test("Test getExplorersByMission() method", () => {
+    const mission = "node";
+    const filteredExplorers = ExplorerController.getExplorersByMission(mission);
+
+    const areExplorerFromNode = filteredExplorers.every(
+      (explorer) => explorer.mission === "node"
+    );
+
+    expect(areExplorerFromNode).toBeTruthy();
+  });
+
+  test("Test getExplorersAmountByMission() method", () => {
+    const mission = "node";
+    const filteredExplorersAmount =
+      ExplorerController.getExplorersAmountByMission(mission);
+
+    expect(filteredExplorersAmount).toBe(10);
+  });
+
+  test("Test getExplorersUsernamesByMission() method", () => {
+    const mission = "node";
+    const filteredExplorersUsernames =
+      ExplorerController.getExplorersUsernamesByMission(mission);
+
+    const expectedList = [
+      "ajolonauta1",
+      "ajolonauta2",
+      "ajolonauta3",
+      "ajolonauta4",
+      "ajolonauta5",
+      "ajolonauta11",
+      "ajolonauta12",
+      "ajolonauta13",
+      "ajolonauta14",
+      "ajolonauta15",
+    ];
+
+    expect(filteredExplorersUsernames).not.toBeUndefined();
+    expect(filteredExplorersUsernames.join()).toBe(expectedList.join());
+  });
+
+  test("Test applyValidationInNumber() method", () => {
+    const numbersToValidate = [1, 3, 5, 15, 20, 23, 30, 33];
+
+    const fizzbuzzList = numbersToValidate.map((number) =>
+      ExplorerController.applyValidationInNumber(number)
+    );
+
+    expect(fizzbuzzList[0]).toBe(numbersToValidate[0]);
+    expect(fizzbuzzList[1]).toBe("FIZZ");
+    expect(fizzbuzzList[2]).toBe("BUZZ");
+    expect(fizzbuzzList[3]).toBe("FIZZBUZZ");
+    expect(fizzbuzzList[4]).toBe("BUZZ");
+    expect(fizzbuzzList[5]).toBe(numbersToValidate[5]);
+    expect(fizzbuzzList[6]).toBe("FIZZBUZZ");
+    expect(fizzbuzzList[7]).toBe("FIZZ");
+  });
+
+  test("Test getExplorersByStack() method", () => {
+    const stack = "javascript";
+    const filteredExplorers = ExplorerController.getExplorersByStack(stack);
+
+    const haveJavascriptStack = filteredExplorers.every((explorer) =>
+      explorer.stacks.includes(stack)
+    );
+
+    expect(haveJavascriptStack).toBeTruthy();
+    expect(filteredExplorers.length).toBe(11);
+  });
+
+  test("Test getStringOfExplorersUsernameByMission() method", () => {
+    const mission = "node";
+    const stringOfFilteredExplorersUsernames =
+      ExplorerController.getStringOfExplorersUsernamesByMission(mission);
+
+    const expectedString = [
+      "ajolonauta1",
+      "ajolonauta2",
+      "ajolonauta3",
+      "ajolonauta4",
+      "ajolonauta5",
+      "ajolonauta11",
+      "ajolonauta12",
+      "ajolonauta13",
+      "ajolonauta14",
+      "ajolonauta15",
+    ].join(", ");
+
+    expect(stringOfFilteredExplorersUsernames).not.toBeUndefined();
+    expect(stringOfFilteredExplorersUsernames).toBe(expectedString);
+  });
+});
